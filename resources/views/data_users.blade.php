910ef88834f59029e0a6e206cf343532cc34def1
Ronald <ronald.syarif@enterprisys.co.id>
2021-06-17 11:38:50 +0700

    re

--- resources/views/data_users.blade.php
+++ resources/views/data_users.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+    <link href="{{ asset('/images/favicon.png') }}" rel="shortcut icon">
     <title>Data Users</title>
 </head>
 <body>
